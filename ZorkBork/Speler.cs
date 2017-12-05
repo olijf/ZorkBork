fe32fc2d2033ba8f26fa746d7295b560a06c678c
Michael <freakinengineering0@gmail.com>
2017-12-05 15:26:51 +0100

    Score toegevoegd player class

---
+++ ZorkBork/Speler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Speler
+{
+    private int score;
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public Speler()
+	{
+        score = 0;
+	}
+
+    public void verhoogScore(int hoeveelheid)
+    {
+        score = score + hoeveelheid;
+    }
+
+    public void verlaagScore(int hoeveelheid)
+    {
+        int tempScore = score - hoeveelheid;
+        if(tempScore >= 0)
+        {
+            score = tempScore;
+        } else
+        {
+            score = 0;
+        }
+    }
+
+    public void resetScore()
+    {
+        score = 0;
+    }
+
+}
---
+++ ZorkBorkTestProject/UnitTestSpeler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZorkBorkTestProject
+{
+    [TestClass]
+    public class UnitTestSpeler
+    {
+        [TestMethod]
+        public void SpelerClassExists()
+        {
+            var speler = new Speler();
+            Assert.IsNotNull(speler);
+        }
+
+        [TestMethod]
+        public void PlayerScoreIsZero()
+        {
+            var speler = new Speler();
+            Assert.AreEqual(0, speler.Score);
+        }
+
+        [TestMethod]
+        public void VerhoogScore()
+        {
+            var speler = new Speler();
+            speler.verhoogScore(100);
+            Assert.AreEqual(100, speler.Score);
+        }
+
+        [TestMethod]
+        public void VerlaagScorePositief()
+        {
+            var speler = new Speler();
+            speler.verhoogScore(100);
+            speler.verlaagScore(50);
+            Assert.AreEqual(50, speler.Score);
+            speler.verlaagScore(50);
+            Assert.AreEqual(0, speler.Score);
+        }
+
+        [TestMethod]
+        public void VerlaagScoreNegatief()
+        {
+            var speler = new Speler();
+            speler.verlaagScore(50);
+            Assert.AreEqual(0, speler.Score);
+        }
+
+        [TestMethod]
+        public void ResetScore()
+        {
+            var speler = new Speler();
+            speler.verhoogScore(50);
+            speler.resetScore();
+            Assert.AreEqual(0, speler.Score);
+        }
+    }
+}
