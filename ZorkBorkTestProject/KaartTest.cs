2bb5c7fb49ce54fddf6045744d571c38c1863160
Michael <freakinengineering0@gmail.com>
2017-12-11 15:34:38 +0100

    Testmethod navigatie uitbreiding

--- ZorkBorkTestProject/KaartTest.cs
+++ ZorkBorkTestProject/KaartTest.cs
@@ -71,10 +71,31 @@ namespace ZorkBorkTestProject
                 };
                 var voorNieuwePositie = Kaart.Instance.Positie;
                 Kaart.Instance.UpdatePositie(Richting.Omhoog);
-                Assert.AreNotEqual(voorNieuwePositie, Kaart.Instance.Positie);
+                Assert.AreNotSame(voorNieuwePositie, Kaart.Instance.Positie);
+            }
+        }
 
+        [TestMethod]
+        public void CheckOmhoog()
+        {
+            using (ShimsContext.Create())
+            {
+                ZorkBork.Fakes.ShimSettings.GetValueString = (_) =>
+                {
+                    return @"..\..\..\MapFinal.xml";
 
+                };
 
+                ZorkBork.Fakes.ShimSettings.GetValueAsIntString = (_) =>
+                {
+                    return 10;
+
+                };
+                var voorOmhoog = Kaart.Instance.Positie;
+                voorOmhoog.y += 1;
+                Kaart.Instance.UpdatePositie(Richting.Omhoog);
+                
+                Assert.IsTrue(voorOmhoog.Equals(Kaart.Instance.Positie));
             }
         }
     }
