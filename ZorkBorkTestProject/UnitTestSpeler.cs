dd248252fc51bcf49f55af643f7ec6960f24f5e2
Michael <freakinengineering0@gmail.com>
2017-12-12 16:03:09 +0100

    Unittests updated

--- ZorkBorkTestProject/UnitTestSpeler.cs
+++ ZorkBorkTestProject/UnitTestSpeler.cs
@@ -25,7 +25,7 @@ namespace ZorkBorkTestProject
         public void VerhoogScore()
         {
             var speler = new Speler();
-            speler.VerhoogScore(100);
+            speler.VerhoogOfVerlaagScore(100);
             Assert.AreEqual(100, speler.Score);
         }
 
@@ -33,8 +33,8 @@ namespace ZorkBorkTestProject
         public void VerlaagScorePositief()
         {
             var speler = new Speler();
-            speler.VerhoogScore(100);
-            speler.VerlaagScore(50);
+            speler.VerhoogOfVerlaagScore(100);
+            speler.VerhoogOfVerlaagScore(-50);
             Assert.AreEqual(50, speler.Score);
         }
 
@@ -42,18 +42,67 @@ namespace ZorkBorkTestProject
         public void VerlaagScoreKanNietNegatief()
         {
             var speler = new Speler();
-            speler.VerlaagScore(50);
-            speler.VerlaagScore(100);
+            speler.VerhoogOfVerlaagScore(-50);
+            speler.VerhoogOfVerlaagScore(-100);
             Assert.AreEqual(0, speler.Score);
         }
 
         [TestMethod]
-        public void ResetScore()
+        public void VeranderHealth()
         {
             var speler = new Speler();
-            speler.VerhoogScore(50);
-            speler.ResetScore();
+            speler.VerhoogOfVerlaagHealth(-50);
+            speler.VerhoogOfVerlaagHealth(25);
+            Assert.AreEqual(75, speler.Health);
+        }
+
+        [TestMethod]
+        public void HealthBoundCheckKleinerDanNul()
+        {
+            var speler = new Speler();
+            speler.VerhoogOfVerlaagHealth(-500);
+            Assert.AreEqual(0, speler.Health);
+        }
+
+        [TestMethod]
+        public void HealthBoundCheckGroterDanHonderd()
+        {
+            var speler = new Speler();
+            speler.VerhoogOfVerlaagHealth(500);
+            Assert.AreEqual(100, speler.Health);
+        }
+
+        [TestMethod]
+        public void VoegSleutelToe()
+        {
+            var speler = new Speler();
+            speler.VoegSleutelToe("Sleutel");
+            Assert.AreEqual("Sleutel", speler.Sleutels[0]);
+        }
+
+        [TestMethod]
+        public void ResetSpelerScoreCheck()
+        {
+            var speler = new Speler();
+            speler.VerhoogOfVerlaagScore(50);
+            speler.ResetSpeler();
             Assert.AreEqual(0, speler.Score);
+        }
+
+        public void ResetSpelerHealthCheck()
+        {
+            var speler = new Speler();
+            speler.VerhoogOfVerlaagHealth(-50);
+            speler.ResetSpeler();
+            Assert.AreEqual(0, speler.Health);
+        }
+
+        public void ResetSpelerSleutelCheck()
+        {
+            var speler = new Speler();
+            speler.VoegSleutelToe("Sleutel");
+            speler.ResetSpeler();
+            Assert.AreEqual(0, speler.Sleutels.Count);
         }
     }
 }
