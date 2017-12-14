0f5b9c17b1bcd55425635c1a1b87fb92a4867a71
Michael <freakinengineering0@gmail.com>
2017-12-14 13:40:56 +0100

    Nog meer unittests aded

---
+++ ZorkBorkTestProject/UnitTestsHealth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZorkBork;
+
+namespace ZorkBorkTestProject
+{
+    [TestClass]
+    public class UnitTestsHealth
+    {
+
+        [TestMethod]
+        public void CheckHealthVerhogingNaPickup()
+        {
+            var speler = new Speler();
+            var healthPickup = new HealthPickup();
+
+            healthPickup.Interact(speler);
+
+            Assert.AreEqual(100, speler.Health);
+        }
+    }
+}
---
+++ ZorkBorkTestProject/UnitTestsScore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZorkBork;
+
+namespace ZorkBorkTestProject
+{
+
+    [TestClass]
+    public class UnitTestsScore
+    {
+        [TestMethod]
+        public void CheckScoreverhoging()
+        {
+            var speler = new Speler();
+            var scorePickup = new ScorePickup();
+
+            scorePickup.Interact(speler);
+
+            Assert.AreEqual(100, speler.Score);
+        }
+    }
+}
