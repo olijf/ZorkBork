1896c6b27792e22faae7c17cb705ca8fc59806a1
Michael <freakinengineering0@gmail.com>
2017-12-15 14:42:26 +0100

    Laatste versie

--- ZorkBork/HealthPickup.cs
+++ ZorkBork/HealthPickup.cs
@@ -14,7 +14,7 @@ namespace ZorkBork
         public override void Interact(Speler speler)
         {
             speler.VerhoogOfVerlaagHealth(100);
-            Console.WriteLine("Score: {0}\nNieuwe health: {1}", speler.Score, speler.Health);
+            ConsoleWrapper.WriteLine("je hebt een health pickup gescoord!!!");
         }
     }
 }
